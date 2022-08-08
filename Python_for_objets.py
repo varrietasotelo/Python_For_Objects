47f6302b5c0b3ad8681b52b7c43b5443ba1c4120
unknown <valen65a@hotmail.com>
2022-08-08 13:26:01 -0500

    add random_str

---
+++ Python_for_objets.py
@@ -0,0 +1,27 @@
+'''
+item1 = "prhone"
+item1_price = 100
+item1_quantity = 5
+item1_price_total = item1_price * item1_quantity
+
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quantity))
+print(type(item1_price_total))
+
+#Ejercicio inicial del video visto en clase 
+
+'''
+
+class Item:
+    pass
+
+item1 = Item()
+item1.name = "phone" 
+item1.price = 100
+item1.quantity = 5
+
+random_str = "cccc"
+
+print(random_str.upper)
