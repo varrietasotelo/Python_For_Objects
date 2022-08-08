d0e0fc237201877f07ffd2da4d0d0324918f494d
unknown <valen65a@hotmail.com>
2022-08-08 13:41:08 -0500

    we have made one function

--- Python_for_objets.py
+++ Python_for_objets.py
@@ -15,13 +15,23 @@ print(type(item1_price_total))
 '''
 
 class Item:
-    pass
+    def calculate_total_price(self, x, y):
+        return x * y
+
+#recordar: siempre colocar nombres especificos a las variables, X y Y no es aceptable. 
+#en el caso de item1, la X hara referencia a item1.price y la Y hara referencia a item1.quantity 
 
 item1 = Item()
-item1.name = "phone" 
+item1.name = "Phone" 
 item1.price = 100
 item1.quantity = 5
+print(item1.calculate_total_price(item1.price, item1.quantity))
 
-random_str = "cccc"
+random_str = "aaa"
+print(random_str.upper())
 
-print(random_str.upper)
+item2 = Item()
+item2.name = "Laptop" 
+item2.price = 1000
+item2.quantity = 3
+print(item1.calculate_total_price(item2.price, item2.quantity))
